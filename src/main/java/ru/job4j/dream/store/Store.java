2dcb1eb8a7aeb0659f1c8e33686c7067b2eb7934
kirillkrohmal <krohmal_kirill@mail.ru>
2020-06-09 10:04:34 +0300

    2. Scriplet. Динамическая таблица.

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -3,7 +3,10 @@ package ru.job4j.dream.store;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -12,26 +15,17 @@ public class Store {
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
-    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
-
     private Store() {
-        posts.put(1, new Post(1, "Junior Java Job"));
-        posts.put(2, new Post(2, "Middle Java Job"));
-        posts.put(3, new Post(3, "Senior Java Job"));
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
+        posts.put(1, new Post(1, "Junior Java Job", "Junior Java Job", "2020-11-11"));
+        posts.put(2, new Post(2, "Middle Java Job", "Middle Java Job", "2020-02-13"));
+        posts.put(3, new Post(3, "Senior Java Job", "Senior Java Job", "2020-05-20"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAllPosts() {
+    public Collection<Post> findAll() {
         return posts.values();
-    }
-
-    public Collection<Candidate> findAllCandidates() {
-        return candidates.values();
     }
 }
